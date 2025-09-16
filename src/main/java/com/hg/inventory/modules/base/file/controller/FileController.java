41f43b79b602d743d2cdad8095f8a5f884559981
mybetatheta <gtredcvf@163.com>
2025-09-16 10:23:49 +0800

    文件上传相关

---
+++ src/main/java/com/hg/inventory/modules/base/file/controller/FileController.java
@@ -0,0 +1,89 @@
+package com.hg.inventory.modules.base.file.controller;
+
+import com.hg.inventory.common.domain.vo.MapResult;
+import com.hg.inventory.common.file.config.FileServerConfig;
+import com.hg.inventory.common.utils.StringUtils;
+import com.hg.inventory.common.utils.file.FileUploadUtils;
+import com.hg.inventory.common.utils.file.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 通用请求处理
+ *
+ * @author zy
+ */
+// @SaIgnore
+@RestController
+@RequestMapping("/file")
+public class FileController {
+    private static final Logger log = LoggerFactory.getLogger(FileController.class);
+
+    @Autowired
+    private FileServerConfig serverConfig;
+
+    private static final String FILE_DELIMETER = ",";
+
+    /**
+     * 通用上传请求（单个）
+     */
+    @PostMapping("/upload")
+    public MapResult uploadFile(MultipartFile file) throws Exception {
+        try {
+            // 上传文件路径
+            String filePath = serverConfig.getUploadPath();
+            // 上传并返回新文件名称
+            String fileName = FileUploadUtils.upload(filePath, file);
+            // String url = serverConfig.getUrl() + fileName;
+            String url = serverConfig.getUrl() + serverConfig.getUrlPrefix() + fileName;
+            MapResult ajax = MapResult.success();
+            ajax.put("url", url);
+            ajax.put("fileName", fileName);
+            ajax.put("newFileName", FileUtils.getName(fileName));
+            ajax.put("originalFilename", file.getOriginalFilename());
+            return ajax;
+        } catch (Exception e) {
+            return MapResult.error(e.getMessage());
+        }
+    }
+
+    /**
+     * 通用上传请求（多个）
+     */
+    @PostMapping("/uploads")
+    public MapResult uploadFiles(List<MultipartFile> files) throws Exception {
+        try {
+            // 上传文件路径
+            String filePath = myProjectConfig.getUploadPath();
+            List<String> urls = new ArrayList<String>();
+            List<String> fileNames = new ArrayList<String>();
+            List<String> newFileNames = new ArrayList<String>();
+            List<String> originalFilenames = new ArrayList<String>();
+            for (MultipartFile file : files) {
+                // 上传并返回新文件名称
+                String fileName = FileUploadUtils.upload(filePath, file);
+                String url = serverConfig.getUrl() + fileName;
+                urls.add(url);
+                fileNames.add(fileName);
+                newFileNames.add(FileUtils.getName(fileName));
+                originalFilenames.add(file.getOriginalFilename());
+            }
+            MapResult ajax = MapResult.success();
+            ajax.put("urls", StringUtils.join(urls, FILE_DELIMETER));
+            ajax.put("fileNames", StringUtils.join(fileNames, FILE_DELIMETER));
+            ajax.put("newFileNames", StringUtils.join(newFileNames, FILE_DELIMETER));
+            ajax.put("originalFilenames", StringUtils.join(originalFilenames, FILE_DELIMETER));
+            return ajax;
+        } catch (Exception e) {
+            return MapResult.error(e.getMessage());
+        }
+    }
+}
